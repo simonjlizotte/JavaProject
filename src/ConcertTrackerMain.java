898db6b00f527742b18564974b073cc044c3f3cc
SimonLizotte <simon.lizotte01@stclairconnect.ca>
2017-12-03 14:39:43 -0500

    Initial commit ISSUE #6 * Added all libraries and javafx compatabilities * Have an empty javafx project ready to be branched off of * will create a staging shortly after this to complete

---
+++ src/ConcertTrackerMain.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ConcertTrackerMain extends Application{
+
+	public static void main(String[] args)  {
+		// Launch application with argument args for mac operations	
+		Application.launch(args);
+	}
+	
+	// Implemented methods of Application
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+}
