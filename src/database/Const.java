06ea6c299ad7ab945a20eb539dc3ffb4bf886658
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-12-11 12:04:29 -0500

    #16 finished setting up the database. created a class called Const so that in future holds the Table constants for names. Also, set up the connection inside the database constructor. Missing: all related to the tables needed.

---
+++ src/database/Const.java
@@ -0,0 +1,16 @@
+package database;
+
+/**
+ * 
+ * Class should hold all the constants for the table and column names
+ *
+ */
+public class Const {
+	
+	//credentials (could be placed in a different class that will be ignored by git)
+		public static final String DB_NAME = "database";
+		public static final String DB_USERNAME = "username";
+		public static final String DB_PASSWORD = "password";
+		
+	//TABLES CONSTANTS SHOULD BE PLACED HERE 
+}
--- src/database/Database.java
+++ src/database/Database.java
@@ -1,25 +1,47 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import Database.Const;
 import database.Database;
 
 /**
+ *
  * 
  * @author josegeorges
  *
  *	the database class will be created with a singleton pattern so that only one connection gets open
+ *
+ *	logs are meant for debugging purposes
  */
 public class Database {
 
+	//TODO CREATE a method to create tables needed.
+	
 	//properties
 	private static Database instance = null;
 	private Connection connection = null; //this is basically the connection between the server and our application
 	
-	//private constructor. so is never called
+	//private constructor. so it is never called
 	private Database() {
-		
+		if(connection == null) {
+			try {
+				//this line is setting up what are we using for the connection
+				Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager.getConnection("jdbc:mysql://php.scweb.ca/"+Const.DB_NAME+"?useSSL=false", Const.DB_USERNAME, Const.DB_PASSWORD);
+				System.out.println("Connection Successfully created");
+			}catch(Exception e) {
+				System.out.println("Something went wrong with the connection");
+				e.printStackTrace();
+			}
+			
+			//CREATE QUERIES FOR TABLES SHOULD BE PLACE HERE.
+			
+		}else {
+			System.out.println("Connection already created");
+		}
 	}
 	
 	
