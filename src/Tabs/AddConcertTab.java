f56b18c67257f5ccd3a917fc7cf85530512f620d
Adam Bazzi <adamjamil.bazzi64@stclairconnect.ca>
2017-12-11 12:09:00 -0500

    Started to work on the AddConcertTab Added a third party Library called ControlFx

--- src/Tabs/AddConcertTab.java
+++ src/Tabs/AddConcertTab.java
@@ -1,6 +1,13 @@
 package Tabs;
 
+import org.controlsfx.control.Rating;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 /**
  * 
@@ -17,9 +24,71 @@ public class AddConcertTab extends Tab{
 	//AddConcertTab created
 	private static AddConcertTab tab;
 	
+	//Create a Raiting variable
+	Rating rating = new Rating();
+	
 	//constructor
 	private AddConcertTab() {
 		this.setText(TAB_TITLE);
+		GridPane pane = new GridPane();
+		
+		//Declaring insets
+		Insets insets = new Insets(10,10,10,10);
+		
+		//Declaring the Rating
+
+		
+		//First Row - Band		
+		Text bandName = new Text("Band:");
+		pane.add(bandName, 0, 0);
+		TextField bandNameInput = new TextField();
+		pane.add(bandNameInput, 1, 0);
+		
+		//Second Row - Venue
+		Text venue = new Text("Venue: ");
+		pane.add(venue, 0, 1);
+		TextField venueInput = new TextField();
+		pane.add(venueInput, 1, 1);
+		
+		//Third Row - City
+		Text city = new Text("City: ");
+		pane.add(city, 0, 2);
+		TextField cityInput = new TextField();
+		pane.add(cityInput, 1, 2);
+		
+		//Fourth Row - Opening Act
+		Text openingAct = new Text("Opening Act: ");
+		pane.add(openingAct, 0, 3);
+		TextField openingActInput = new TextField();
+		pane.add(openingActInput, 1, 3);
+		
+		//Fifth Row - Textfield for the genre, will change to a combobox when the ENUMS are made
+		Text genre = new Text("Genre:");
+		pane.add(genre, 0, 4);
+		TextField genreInput = new TextField();
+		pane.add(genreInput, 1, 4);
+		
+		//Sixth Row - Seat Numbers
+		Text seatNumbers = new Text("Seat Numbers: ");
+		TextField seatNumbersInput = new TextField();
+		pane.add(seatNumbers, 0, 5);
+		pane.add(seatNumbersInput, 1, 5);
+		
+		//Seventh Row - Date attended
+		Text dateAttended = new Text("Date Attended: ");
+		DatePicker date = new DatePicker();
+		pane.add(dateAttended, 0, 6);
+		pane.add(date, 1, 6);
+		
+		
+		//Eighth Row - Rating
+		pane.add(rating, 0, 7);
+		
+		
+		pane.setPadding(insets);
+		pane.setVgap(10);
+		pane.setHgap(10);
+		this.setContent(pane);
 	}
 	
 	//this method will be call when needing the instance of the tab or when first creating it
