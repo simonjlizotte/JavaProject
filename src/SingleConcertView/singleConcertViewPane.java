7eb9f7c10a2bf3c5780650eb2fe3df9d085a15cc
Nicholas Stajduhar <nicholas.stajduhar01@stclairconnect.ca>
2017-12-12 14:55:02 -0500

    #15 Created the Edit feature *Added a new button and label when the editing process is activated *Added an event listener for the edit button *Set certain values visible and some hidden depending on the requirements *Set editable to false for all TextFields when not in edit mode

--- src/SingleConcertView/singleConcertViewPane.java
+++ src/SingleConcertView/singleConcertViewPane.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import javax.sound.sampled.LineUnavailableException;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -35,9 +37,16 @@ public class singleConcertViewPane extends BorderPane {
 		
 		//Creating the remove and edit buttons
 		Button edit = new Button("Update Concert");
+		edit.setVisible(true);
 		Button remove = new Button("Remove Concert");
+		Button saveEdits = new Button("Save Changes");
+		saveEdits.setVisible(false);
 		
-		buttonBox.getChildren().addAll(edit, remove);
+		Label updatesValues = new Label("Update The Values");
+		inputs.add(updatesValues, 0, 0);
+		updatesValues.setVisible(false);
+		
+		buttonBox.getChildren().addAll(edit, remove, saveEdits);
 		buttonBox.setAlignment(Pos.CENTER);
 		buttonBox.setPadding(new Insets(10,10,10,10));
 		
@@ -47,43 +56,88 @@ public class singleConcertViewPane extends BorderPane {
 		
 		//Creating the input fields labels
 		Label whatBand = new Label("What band?");
-		inputs.add(whatBand, 0, 0);
+		inputs.add(whatBand, 0, 1);
 		Label whereAt = new Label("What Venue?");
-		inputs.add(whereAt, 0, 1);
+		inputs.add(whereAt, 0, 2);
 		Label whatCity = new Label("What City?");
-		inputs.add(whatCity, 0, 2);
+		inputs.add(whatCity, 0, 3);
 		Label openingAct = new Label("Who was the opening act?");
-		inputs.add(openingAct, 0, 3);
+		inputs.add(openingAct, 0, 4);
 		Label genere = new Label("Genere:");
-		inputs.add(genere, 0, 4);
+		inputs.add(genere, 0, 5);
 		Label seats = new Label("Seat Numbers & Section:");
-		inputs.add(seats, 0, 5);
+		inputs.add(seats, 0, 6);
 		Label dateAttended = new Label("Date Attended:");
 		Label dateFormat = new Label("dd/mm/yyyy");
-		inputs.add(dateAttended, 0, 6);
-		inputs.add(dateFormat, 2, 6);
+		inputs.add(dateAttended, 0, 7);
+		inputs.add(dateFormat, 2, 7);
 		Label overallRating = new Label("Overall Rating:");
-		inputs.add(overallRating, 0, 7);
+		inputs.add(overallRating, 0, 8);
 		Label pictures = new Label("Pictures from the Event:");
-		inputs.add(pictures, 0, 8);
+		inputs.add(pictures, 0, 9);
 		
 		//Create the TextFields, DatePicker, and ComboBox for the values
-		TextField whatBandInput = new TextField("Band Name");
-		inputs.add(whatBandInput, 1, 0);
-		TextField whereAtInput = new TextField("Venue Name");
-		inputs.add(whereAtInput, 1, 1);
-		TextField whatCityInput = new TextField("City");
-		inputs.add(whatCityInput, 1, 2);
-		TextField openingActInput = new TextField("Opening Act");
-		inputs.add(openingActInput, 1, 3);
+		TextField whatBandInput = new TextField();
+		whatBandInput.setPromptText("Band Name");
+		whatBandInput.setEditable(false);
+		inputs.add(whatBandInput, 1, 1);
+		TextField whereAtInput = new TextField();
+		whereAtInput.setPromptText("Venue Name");
+		whereAtInput.setEditable(false);
+		inputs.add(whereAtInput, 1, 2);
+		TextField whatCityInput = new TextField();
+		whatCityInput.setEditable(false);
+		whatCityInput.setPromptText("City");
+		inputs.add(whatCityInput, 1, 3);
+		TextField openingActInput = new TextField();
+		openingActInput.setEditable(false);
+		openingActInput.setPromptText("Opening Act");
+		inputs.add(openingActInput, 1, 4);
 		ComboBox<ArrayList> genereInput = new ComboBox<>();
-		inputs.add(genereInput, 1, 4);
-		TextField seatsInput = new TextField("Seats and Section");
-		inputs.add(seatsInput, 1, 5);
+		inputs.add(genereInput, 1, 5);
+		TextField seatsInput = new TextField();
+		seatsInput.setEditable(false);
+		seatsInput.setPromptText("Seats and Section");
+		inputs.add(seatsInput, 1, 6);
 		DatePicker dateAttendedInput = new DatePicker();
-		inputs.add(dateAttendedInput, 1, 6);
-		TextField overallRatingInput = new TextField("Rating");
-		inputs.add(overallRatingInput, 1, 7);
+		inputs.add(dateAttendedInput, 1, 7);
+		TextField overallRatingInput = new TextField();
+		overallRatingInput.setEditable(false);
+		overallRatingInput.setPromptText("Rating");
+		inputs.add(overallRatingInput, 1, 8);
+		
+		
+		edit.setOnAction((event)->{
+			
+			whatBandInput.setEditable(true);
+			whereAtInput.setEditable(true);
+			whatCityInput.setEditable(true);
+			openingActInput.setEditable(true);
+			seatsInput.setEditable(true);
+			overallRatingInput.setEditable(true);
+			
+			edit.setVisible(false);
+			saveEdits.setVisible(true);
+			updatesValues.setVisible(true);
+			
+		});
+		
+		saveEdits.setOnAction((event)->{
+					
+					whatBandInput.setEditable(false);
+					whereAtInput.setEditable(false);
+					whatCityInput.setEditable(false);
+					openingActInput.setEditable(false);
+					seatsInput.setEditable(false);
+					overallRatingInput.setEditable(false);
+					
+					edit.setVisible(true);
+					saveEdits.setVisible(false);
+					updatesValues.setVisible(false);
+					
+				});
+		
+		
 		
 		// add nodes to pane 
 		
