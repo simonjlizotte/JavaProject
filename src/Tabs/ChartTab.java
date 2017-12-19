333f695277b62f2207f3c186e8e42b9782095456
carmenkerim12 <carmen.kerim01@stclairconnect.ca>
2017-12-19 13:26:31 -0500

    Created 2 other sample datas to populate the charts to see how they would look

--- src/Tabs/ChartTab.java
+++ src/Tabs/ChartTab.java
@@ -2,6 +2,7 @@ package Tabs;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.Tab;
@@ -39,16 +40,45 @@ public class ChartTab extends Tab{
 		// pieChart for year
 		PieChart yearChart = new PieChart();
 		
-		yearChart.setTitle("All Coins Found");
-		yearChart.setLabelsVisible(true);
 		
+		citiesChart.setTitle("Cities");
+		citiesChart.setLabelsVisible(true);
+		//Sample obserableList for the cities
+				ObservableList<PieChart.Data> citiesData = 
+						FXCollections.observableArrayList(
+								new PieChart.Data("Windsor", 2));	
+		//Set the data to the citiesChart
+		citiesChart.setData(citiesData);
+		
+		genresChart.setTitle("Genres");
+		genresChart.setLabelsVisible(true);
+		//Sample obserableList for the cities
+				ObservableList<PieChart.Data> genresData = 
+						FXCollections.observableArrayList(
+								new PieChart.Data("Hard", 2));	
+		//Set the data to the citiesChart
+		genresChart.setData(genresData);
+		
+		//Title to the yearChart
+		yearChart.setTitle("Year");
+		yearChart.setLabelsVisible(true);
+		//Sample obserableList for the yearChart
 		ObservableList<PieChart.Data> yearData = 
 				FXCollections.observableArrayList(
-						new PieChart.Data("Hi", 2));
-						
+						new PieChart.Data("2007", 2));	
+		//Set the data to the yearChart
 		yearChart.setData(yearData);
 		
+		pane.setHgap(10);
+		pane.setPadding(new Insets(10,10,10,10));
+		//Added yearChart to pane
 		pane.add(yearChart, 0, 0);
+		pane.add(genresChart, 0, 1);
+		pane.add(citiesChart, 1, 0);
+		
+		
+		
+		
 		
 		this.setContent(pane);
 		
