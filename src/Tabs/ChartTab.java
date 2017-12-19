8d478c08a5c76ff5ed1c0a30caa336af46f685d4
carmenkerim12 <carmen.kerim01@stclairconnect.ca>
2017-12-19 13:20:35 -0500

    Added 3 PieCharts to the ChartTab. CitiesCart,GenresChart, and yearChart. I than created a sample observableList to test that the charts were in the right view

--- src/Tabs/ChartTab.java
+++ src/Tabs/ChartTab.java
@@ -1,6 +1,11 @@
 package Tabs;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.GridPane;
 
 /**
  * 
@@ -9,8 +14,11 @@ import javafx.scene.control.Tab;
  * This tab is designed to follow a singleton pattern, meaning that there will only be access
  * to one instance of this tab.
  */
-public class ChartTab extends Tab{
 
+public class ChartTab extends Tab{
+	public static GridPane pane;
+	
+	
 	//constants needed
 	public static final String TAB_TITLE = "Charts"; //title for the tab
 	
@@ -20,6 +28,30 @@ public class ChartTab extends Tab{
 	//constructor
 	private ChartTab() {
 		this.setText(TAB_TITLE);
+		pane = new GridPane();
+		
+		// pieChart for cities
+		PieChart citiesChart = new PieChart();
+		
+		// pieChart for genre
+		PieChart genresChart = new PieChart();
+		
+		// pieChart for year
+		PieChart yearChart = new PieChart();
+		
+		yearChart.setTitle("All Coins Found");
+		yearChart.setLabelsVisible(true);
+		
+		ObservableList<PieChart.Data> yearData = 
+				FXCollections.observableArrayList(
+						new PieChart.Data("Hi", 2));
+						
+		yearChart.setData(yearData);
+		
+		pane.add(yearChart, 0, 0);
+		
+		this.setContent(pane);
+		
 	}
 	
 	//this method will be call when needing the instance of the tab or when first creating it
