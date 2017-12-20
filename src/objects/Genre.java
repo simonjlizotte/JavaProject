2db9034a524cd8db581cd05998755590f102b9e0
SimonLizotte <simon.lizotte01@stclairconnect.ca>
2017-12-19 20:19:31 -0500

    Have started on issue #39 #40 #41 which is database work.  have done this with packagaes and implemented daos etc

---
+++ src/objects/Genre.java
@@ -0,0 +1,46 @@
+package objects;
+/**
+ * 
+ * @author simonlizotte
+ * Genre javabean
+ */
+
+public class Genre {
+
+	// properties
+	private int ID;
+	private String genre;
+	
+	// Default constructor 
+	public Genre() {
+		
+	}
+	
+	public Genre(String genre) {
+		this.genre = genre;
+	}
+	
+	public Genre(int id, String genre) {
+		this.ID = id;
+		this.genre = genre;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	
+	
+}
---
+++ src/tables/concertTable.java
@@ -0,0 +1,40 @@
+package tables;
+
+import java.util.ArrayList;
+
+import DAOS.concertDAO;
+import objects.Concert;
+
+public class concertTable implements concertDAO {
+
+	@Override
+	public ArrayList<Concert> getAllItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Concert getItem(int itemID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateItem(Concert item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteItem(Concert item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void createItem(Concert item) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
