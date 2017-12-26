1db60544279af1fa2a4ee931bfb4cbdd380a1464
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-12-26 13:14:58 -0500

    #40 GenreTable class skeleton created

---
+++ src/Tables/GenreTable.java
@@ -0,0 +1,42 @@
+package Tables;
+
+import java.util.ArrayList;
+
+import DAOS.GenreDAO;
+import objects.Genre;
+
+public class GenreTable implements GenreDAO{
+
+	
+	
+	@Override
+	public void createGenre(Genre genre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Genre> getAllGenres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Genre getGenre(int genreID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateGenre(Genre genre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteGenre(Genre genre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
