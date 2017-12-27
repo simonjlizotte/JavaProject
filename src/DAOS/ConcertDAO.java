9270c62dafdda494f18a7b8bd42e466908d791af
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-12-27 17:53:08 -0500

    #39 added all the crud methods for the ConcertDAO

--- src/DAOS/ConcertDAO.java
+++ src/DAOS/ConcertDAO.java
@@ -1,5 +1,21 @@
 package DAOS;
 
-public interface ConcertDAO {
+import java.util.ArrayList;
 
+import objects.Band;
+import objects.Concert;
+import objects.Genre;
+
+public interface ConcertDAO {
+		//Create
+		public void createConcert(Concert concert, Band band, Genre genre);
+		//Read
+		public ArrayList<Concert> getAllConcerts();
+		public Band getConcert(int concertID);
+		//Update
+		public void updateDate(Band band);
+		public void updateRating(Band band);
+		public void updatePicture(Band band);
+		//Delete
+		public void deleteBand(Band band);
 }
