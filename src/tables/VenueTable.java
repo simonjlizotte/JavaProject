01cd2faedce86798f6525516cb173e74c542d3a3
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-12-27 17:28:38 -0500

    #40 VenueTable now implements VenueDAO

--- src/tables/VenueTable.java
+++ src/tables/VenueTable.java
@@ -1,5 +1,40 @@
 package tables;
 
-public class VenueTable {
+import java.util.ArrayList;
+
+import DAOS.VenueDAO;
+import objects.Venue;
+
+public class VenueTable implements VenueDAO{
+
+	@Override
+	public void createGenre(Venue venue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Venue> getAllVenues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Venue getVenue(int venueID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateVenue(Venue venue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteVenue(Venue venue) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
