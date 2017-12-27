44a66aff5fcb1dc289a5538106d15278ce21a199
Jose Georges <joseantonio.georgesboutros01@stclairconnect.ca>
2017-12-27 17:27:38 -0500

    #40 added crud methods for the VenueDAO

--- src/DAOS/VenueDAO.java
+++ src/DAOS/VenueDAO.java
@@ -1,5 +1,17 @@
 package DAOS;
 
-public interface VenueDAO {
+import java.util.ArrayList;
 
+import objects.Venue;
+
+public interface VenueDAO {
+		//Create
+		public void createGenre(Venue venue);
+		//Read
+		public ArrayList<Venue> getAllVenues();
+		public Venue getVenue(int venueID);
+		//Update
+		public void updateVenue(Venue venue);
+		//Delete
+		public void deleteVenue(Venue venue);
 }
